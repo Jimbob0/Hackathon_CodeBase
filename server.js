778f3fcaef79a439e6e881e6eb9c543db456ea47
Jimbob0 <jneal3@citadel.edu>
2016-08-03 14:47:36 -0600

    Added login api

--- server.js
+++ server.js
@@ -15,9 +15,13 @@ var PORT = process.env.port || 8000;
 
 var bodyParser = require("body-parser");
 
+var session = require('express-session');
+
 var mongoose = require("mongoose");
 
 mongoose.connect('mongodb://localhost');
+
+var UserFtns = require("./UserFtns.js");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -47,7 +51,65 @@ app.get("/", function(req, res) {
 app.post("/api", function(req, res) {
 	res.send("success");
 });
+app.get('/login', function(req, res){
+	res.sendFile(__dirname + "/public/login.html");
+});
 
+app.post('/login', function(req, res){
+	if (UserFtns.checkLogin(req.body.username, req.body.password))
+	{
+		req.session.user = req.body.username;
+		res.send("success");
+	}else {
+		res.send("error");
+	}
+});
+
+app.get('/logout', function(req,res){
+	req.session.user = "";
+	res.redirect("/index.html");
+});
+
+app.get('/new_page(.html)?', function(req,res) {
+	if (!req.session.user) {
+		res.redirect("/login");
+		return;
+	}
+	res.sendFile(__dirname + "/public/new_page.html");
+});
+
+app.post('/register', function(req, res){
+	//shorthand variables to save us time
+	var username = req.body.username;
+	var password = req.body.password;
+	if (UserFtns.userExists(username)) {
+		// If the username already exists
+		if (UserFtns.checkLogin(username, password)) {
+			// ... and they have the right password
+			// then log the user in
+			req.session.user = username;
+			// Send "success" so that the frontend knows
+			// it is ok to redirect to /map
+			res.send("success");
+		} else {
+			// Otherwise, they might be trying to
+			// take a username that already exists - error!
+			res.send("error");
+		}
+	} else {
+		// Username is not taken, register a new user
+		// and log them in - success!
+		if(UserFtns.registerUser(username, password)) {
+			req.session.user = username;
+			// Send "success" so that the frontend knows
+			// it is ok to redirect to /map
+			res.send("success");
+		} else {
+			// there was a problem registering
+			res.send("error");
+		}
+	}
+});
 // if we want to serve static files out of ./public
 app.use(express.static("public"));
 
